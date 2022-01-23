f4648b54bc12545890cb6ede4340119de502945c
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2022-01-23 16:57:46 +0300

    Add ThemeSwitch component. Update Navbar

--- src/components/base/Nav.jsx
+++ src/components/base/Nav.jsx
@@ -1,17 +1,50 @@
 import { changeTheme } from "../../reduxStore/theme";
 import { useSelector, useDispatch } from "react-redux";
+import theme from "../../theme";
+import ThemeSwitch from "../ThemeSwitch";
 
 const Nav = () => {
-  const theme = useSelector((state) => state.theme);
+  const stateTheme = useSelector((state) => state.theme);
   const dispatch = useDispatch();
-  console.log(theme);
+  const currentTheme = stateTheme ? theme.isDark : theme.isLight;
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button onClick={() => dispatch(changeTheme())}>Change theme</button>
+      <nav className={`navbar navbar-expand-lg ${currentTheme.nav}`}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Capstone Project
+          </a>
+          <div class={`dropdown me-auto`}>
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Movies
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Popular
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Top Rated
+                </a>
+              </li>
+            </ul>
+          </div>
+          <ThemeSwitch onClick={() => dispatch(changeTheme())}></ThemeSwitch>
+          <p className={`my-auto ${currentTheme.p}`}>Avatar</p>
+        </div>
       </nav>
     </>
   );
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  isDark: {
+    nav: 'navbar-dark bg-dark',
+    p: 'text-warning',
+    card: 'text-white bg-primary'
+  },
+  isLight: {
+    nav: 'navbar-light, bg-light',
+    p: 'text-success',
+    card: 'text-white bg-dark'
+  }
+}
+
+export default theme
