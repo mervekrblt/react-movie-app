8d9c4d750ccbd1d84ad34ee4c321fb022a8c9813
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2022-02-01 01:52:51 +0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/base/Nav";
+import Footer from "./components/base/Footer"
 import routes from "./routes";
 import Error from "./pages/Error";
 import LoginError from "./pages/LoginError";
-import Home from "./pages/Home"
-import Login from "./pages/Login"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   const theme = useSelector((state) => state.theme);
   const isLogin = useSelector((state) => state.isLogin);
 
-  document.querySelector("body").setAttribute("id","scrollbar")
+  document.querySelector("body").setAttribute("id", "scrollbar");
 
   document.querySelector("body").style.backgroundColor = theme
     ? "#141414"
@@ -30,10 +31,11 @@ function App() {
               element={<route.element />}
             />
           ))}
-          {isLogin && <Route path="/" element={<Home/>}/>}
-          {!isLogin && <Route path="/" element={<Login/>}/>}
-          <Route path="*" element={!isLogin ? <LoginError/> : <Error />}/>
+        {isLogin && <Route path="/" element={<Home />} />}
+        {!isLogin && <Route path="/" element={<Login />} />}
+        <Route path="*" element={!isLogin ? <LoginError /> : <Error />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/base/Footer.jsx
+++ src/components/base/Footer.jsx
@@ -1,4 +1,14 @@
+import theme from "../../theme";
+import { useSelector } from "react-redux";
+
 const Footer = () => {
-  return <>Footer</>;
+  const stateTheme = useSelector((state) => state.theme);
+  const currentTheme = stateTheme ? theme.isDark : theme.isLight;
+  return (
+      <footer className={`text-center mt-3 ${currentTheme.text}`}>
+        <p>MADE WITH BLOOD, SWEAT AND TEARS</p>
+        <p>&copy; Merve Karabulut, 2022</p>
+      </footer>
+  );
 };
 export default Footer;
