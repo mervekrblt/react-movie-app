bdf0f9f7ac241c00a8fbea019d4028566e48bab5
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2022-02-01 02:15:51 +0300

    Add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,19 @@
 const About = () => {
-return <>About</>
-}
-export default About
+  const tech = ["React", "React-Router-Dom", "Axios", "React", "React-Router-Dom", "Axios"]
+  return (
+    <>
+      <div className="container my-5 border">
+        <header className="text-center">
+          <h3>TECHNOLOGIES</h3>
+        </header>
+
+        <main className="px-3 mt-5 text-center">
+          {tech.map(item => (
+            <span key={item} className="badge p-3 me-3 btn-lg rounded-pill bg-dark">{item}</span>
+          ))}
+        </main>
+      </div>
+    </>
+  );
+};
+export default About;
